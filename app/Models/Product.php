a1fc823913116e946f36871d74b623c2f4747573
13Alireza83 <a.akbarshahi83@gmail.com>
2026-08-02 19:41:45 +0400

    Add Product model and migration with category relationship

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'price',
+        'stock',
+        'image',
+        'category_id',
+        'is_active'
+    ];
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
