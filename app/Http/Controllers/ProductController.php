a91db23d8b7750e7f7b189781a53959524ac3daf
13Alireza83 <a.akbarshahi83@gmail.com>
2026-08-29 17:56:49 +0400

    Fix pagination: use paginate() instead of get() in search

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,14 +7,17 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         $search = $request->input('search');
-        if ($search){
-            $products = Product::where('name', 'like', '%' . $search . '%')->get();
-        }else{
+
+        if ($search) {
+            $products = Product::where('name', 'like', '%' . $search . '%')->paginate(10);
+        } else {
             $products = Product::paginate(10);
         }
-        return view('products.index',compact('products'));
+
+        return view('products.index', compact('products'));
     }
     public function show($id){
         $product = Product::findOrFail($id);
